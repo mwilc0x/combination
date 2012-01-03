f5fbfc638d1751cd846a4225894ad2ff65955beb
Mike Green <mike.is.green@gmail.com>
2012-01-02 21:14:55 -0500

    Added config file, removed superfluous connection stuff and only closing the mysql connection at the very end

--- combine.php
+++ combine.php
@@ -15,6 +15,8 @@
    * it and broke the script. 
   */
   
+// Load MySQL config and initialize connection
+require_once('config.php');
 
   //extract variables from the query and calls func insertToDB() to insert data
   function insert() {
@@ -29,6 +31,8 @@
 
       $html = "http://";
 
+	  // What is this doing?
+	  //
       //see if it's a html page and if not, grab the file
       if(substr($i, 0, 7) == $html)
       {
@@ -46,35 +50,14 @@
 
     //$concat = mysql_real_escape_string($concat);
     $text = base64_encode($text); //encode the html data
-    $con = connect();
-    insertToDB($concat, $text, $con);
+    insertToDB($concat, $text);
     return $concat;
    }
     
-
-    //helper function to connect to DB
-    function connect() {
-      /* Connect and setup table in database. 
-       * Tested with MySQL, insert appropriate variables
-      */
-      
-      $con = mysql_connect("localhost","#my_user","#my_pass");
-      if (!$con) 
-      {
-        die("<p>\nCould not connect: </p>" . mysql_error(). "<p>\n\n</p>");
-      }
-      
-      return $con;
-    }
-    
-
     //sets up the table in DB and inserts appropriate data
-    function insertToDB($files, $text, $con)
+    function insertToDB($files, $text)
     {
 
-      // Create table
-      mysql_select_db("files", $con);
-      
       //check if table is already created, if not, create a new table file_data
       $sql = "CREATE TABLE IF NOT EXISTS file_data 
               (
@@ -86,7 +69,7 @@
               )";
 
       // Execute query
-      mysql_query($sql,$con);
+      mysql_query($sql); // mysql will automatically use the 1st open connection. no need for 2nd parameter here.
 
       //SQL injection prevention
       $files = mysql_real_escape_string($files);
@@ -101,23 +84,17 @@
       
         $sql="INSERT INTO file_data (fileNames, fileData) VALUES ('$files', '$text')";
 
-        if (!mysql_query($sql,$con))
+        if (!mysql_query($sql))
         {
           die("Error: " . mysql_error(). "<p>\n\n</p>");
         }
 
         echo "<p>SUCCESSFULLY ADDED RECORD TO DB\n\n</p>";
       }
-      mysql_close($con);
     } 
 
     //called when we want the data back from the DB
     function retrieve($table_name, $fileName) {
-      //echo $fileName;
-      $con = connect();
-      mysql_select_db("files", $con);
-      
-
       //prevent SQL injection
       //$table_name = mysql_real_escape_string($table_name);
       $fileName = mysql_real_escape_string($fileName);      
@@ -156,4 +133,5 @@
 	}
 
 	echo run();
+	mysql_close($my_conn); // don't close SQL connection until the end.
 ?>
