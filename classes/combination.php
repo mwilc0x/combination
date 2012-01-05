b791ab18f4e659f3c9627326003e206b118de408
Mike Green <mike.is.green@gmail.com>
2012-01-05 09:40:03 -0500

    Brought files over from my conflicted branch - db connection class, combination class, and INI config file. Started a new script that uses the OO classes

--- classes/combination.php
+++ classes/combination.php
@@ -8,9 +8,23 @@
  * instantiate a new Combination object, and either create and save, or search 
  * and return its contents.
  */
+
+// This class depends on the FileCombiner utility class.
+require_once("file_combiner.php");
+
 class Combination {
-	public $config;
+	private $connection; // expects an instance of DBConnection
+	private $file_combiner;
+	public $files;
 	public $content;
 	public $content_type;
+
+	public function __construct($conn) {
+		$this->connection = $conn;
+		$this->file_combiner = new FileCombiner;
+	}
+
+	public function cache_and_combine()  {
+	}
 }
 ?>
--- classes/db_connection.php
+++ classes/db_connection.php
@@ -4,11 +4,22 @@
  *
  * This class is a singleton - it is instantiated once at the beginning of our 
  * script and shared among all instances of Combination. It wraps a MySQL 
- * connection and handles loading all of our configuration variables from a 
- * file.
+ * connection and handles loading all of our configuration variables from an 
+ * INI formatted file.
  */
 
 class DBConnection {
-	public $config;
+	private $config;
+	public $connection;
+
+	public __construct($file) {
+		if ($conf = parse_ini_file($file)) {
+			$this->config = parse_ini_file($file);
+			$this->connection = mysql_connect($this->config['host'], $this->config['user'], $this->config['password']);
+			mysql_select_db($this->config['database']);
+		} else {
+			throw new Exception("Could not load database configuration file: $file");
+		}
+	}
 }
 ?>
